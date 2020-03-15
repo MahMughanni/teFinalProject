76dc670bbd4a9e3fda7a7845f74412c614858285
MahMughanni <43685429+MahMughanni@users.noreply.github.com>
2020-03-15 19:41:37 +0200

    Edit 4 "15/3/20"

---
+++ app/src/main/java/com/mahm/finalproject/Activities/ResetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.mahm.finalproject.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mahm.finalproject.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
---
+++ app/src/main/java/com/mahm/finalproject/Activities/UpdatePhoneNumberActivity.java
@@ -0,0 +1,16 @@
+package com.mahm.finalproject.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mahm.finalproject.R;
+
+public class UpdatePhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_phone_number);
+    }
+}
